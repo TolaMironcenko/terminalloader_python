114f79a130be0a3d23ad68b418654400c62c733c
TolaMironcenko <mironcenkotola@gmail.com>
2022-06-29 08:49:23 +0700

    some loaders and redme file

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 import time
+from tqdm import tqdm
+from progress.bar import IncrementalBar
+from termcolor import cprint
 
-for percent in range(100):
-    s = f"[{(percent // 1) * '|'}"
-    s += f"{(100 - (percent // 1)) * '.'}] "
-    s += f"{percent}"
-    print(s, end="\r")
-    time.sleep(0.1)
+
+def main():
+    # without libraries
+    for percent in range(100):
+        s = f"|{(percent // 1) * '█'}"
+        s += f"{(100 - (percent // 1)) * '.'}| "
+        s += f"{percent}"
+        cprint(s, 'green', end="\r")
+        time.sleep(0.1)
+
+    # with library tqdm
+    for i in tqdm(range(10)):
+        time.sleep(1)
+
+    # with library progress
+    mylist = [1,2,3,4,5,6,7,8]
+
+    bar = IncrementalBar('Countdown', max = len(mylist))
+
+    for item in mylist:
+        bar.next()
+        time.sleep(1)
+
+    bar.finish()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        cprint('exited', 'green')
+        exit()
+    except Exception as e:
+        cprint('Error: '+str(e), 'red')
